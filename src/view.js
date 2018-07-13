35a7fa0fc99385802d3cdeb881803d305043d7e2
jyothi <jyotishridhar7@gmail.com>
2018-07-13 17:53:03 +0530

    table view for added meals

--- src/view.js
+++ src/view.js
@@ -1,8 +1,9 @@
 import hh from 'hyperscript-helpers';
 import {h} from 'virtual-dom';
 import { showFormMsg, mealInputMsg, caloriesInputMsg, saveMealMsg } from './update.js';
+import * as R from 'ramda';
 
-const { pre, div, h1, button, form, label, input } = hh(h);
+const { pre, div, h1, button, form, label, input, td, tr, th, tbody, table, thead } = hh(h);
 
 function fieldSet(labelText, inputValue, oninput){
 	return div([
@@ -53,10 +54,59 @@ function formView(dispatch, model){
 				'Add Meal');
 }
 
+function cell(tag, className, value){
+	return tag({className}, value);
+}
+
+const tableHeader = thead([
+	tr([
+		cell(th, 'pa2 tl', "MEAL"),
+		cell(th, 'pa2 tr', "CALORIES"),
+		cell(th, "", "")
+	])
+])
+
+function mealRow(dispatch, className, meal){
+	return tr({className}, [
+		cell(td, 'pa2', meal.description),
+		cell(td, 'pa2 tr', meal.calories),
+		cell(td, 'pa2 tr', [])
+	]);
+}
+
+function totalRow(meals){
+	const total = R.pipe(
+		R.map(meal => meal.calories),
+		R.sum,
+	)(meals);
+	return tr({ className: 'bt b' }, [
+		cell(td, 'pa2 tl', 'Total'),
+		cell(td, 'pa2 tr', total),
+		cell(td, "", "")
+	]);
+}
+
+function mealBody(dispatch, className, meals){
+	const rows = R.map(R.partial(mealRow, [dispatch, 'stripe-dark']), meals);
+	const rowWithTotal = [...rows, totalRow(meals)];
+	return tbody({className}, rowWithTotal);
+}
+
+function tableView(dispatch, meals){
+	if(meals.length === 0){
+		return div({className: 'mv2 i black-50'}, "No meals to display");
+	}
+	return table({className: 'mv2 w-100 collapse'}, [
+		tableHeader,
+		mealBody(dispatch, '', meals)
+	]);
+}
+
 function view (dispatch, model){
 	return div ({ className: 'mw6 center'}, [
 		h1({className: 'f2 pv2 bb'}, 'Calorie Counter'),
 		formView(dispatch, model),
+		tableView(dispatch, model.meals),
 		pre(JSON.stringify(model, null, 2))
 	]);
 }
